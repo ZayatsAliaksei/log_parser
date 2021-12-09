4b01ab9357245a6ee939d558b02766b46e4de69c
Alex <Lehazayac@tut.by>
2021-12-09 11:44:52 +0300

    tests

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,8 +1,49 @@
-require './lib/parser.rb'
+require './lib/reader.rb'
 
-describe '#hello' do
-  it 'greets the world' do
-    parser = Parser.new
-    expect(parser.hello).to eq "Hello! I'm Ruby!"
+RSpec.describe Reader do
+
+  let(:valid_file) { 'spec/files/valid.log' }
+  let(:invalid_file) { 'spec/files/empty.txt' }
+  let(:not_readable_file) { 'spec/files/qqq.zip' }
+  let(:invalid_file_path) { 'a/b/c' }
+  let(:reader) { described_class.new(valid_file) }
+  let(:reader_invalid) { described_class.new(invalid_file_path) }
+  let(:reader_empty) { described_class.new(invalid_file) }
+  let(:not_reader) { described_class.new(not_readable_file) }
+
+  describe 'initialize' do
+    it 'initializes when path is given as an argument' do
+      expect(reader.path).to eq(valid_file)
+    end
+
+    context 'with no logfile argument' do
+      it 'should raise Argument error' do
+        expect { described_class.new() }.to raise_error(ArgumentError)
+      end
+    end
   end
+
+  describe "#valid?" do
+
+    context "when file is missing" do
+      it "should rise File not found" do
+        expect { reader_invalid.valid? }.to raise_error("File not found")
+      end
+    end
+
+    context "when file is empty" do
+      it "should rise Expecting not empty file" do
+        expect { reader_empty.valid? }.to raise_error("Expecting not empty file")
+      end
+    end
+  end
+
+  describe "#read" do
+    context "when output is a hash" do
+      it "returns hash" do
+        expect(reader.read).to be_a(Hash)
+      end
+    end
+  end
+
 end
--- spec/sorter_spec.rb
+++ spec/sorter_spec.rb
@@ -1,17 +1,55 @@
 require 'rspec'
+require './lib/sorter.rb'
 
-describe 'Sorter' do
-  before do
-    # Do nothing
+RSpec.describe Sorter do
+
+  let(:data) do
+    {
+      "/help_page/1": ["126.318.035.038", "929.398.951.889", "722.247.931.582"],
+      "/contact": ["184.123.665.067", "184.123.665.067", "543.910.244.929"]
+    }
   end
-
-  after do
-    # Do nothing
+  let(:unsorted_data) do
+    {
+      "/help_page/1" => 80, "/contact" => 89, "/home" => 78, "/about/2" => 90, "/index" => 82, "/about" => 81
+    }
   end
+  let(:sorter) { described_class.new(data) }
+  let(:value) { 'visits' }
 
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  describe 'initialize' do
+
+    it 'initializes when path is given as an argument' do
+      expect(sorter.data).to eq(data)
+    end
+
+    it 'should raise Argument error' do
+      expect { described_class.new() }.to raise_error(ArgumentError)
     end
   end
+
+  describe '#hash_value_count' do
+    it 'should count sum of each key values' do
+      expect(sorter.hash_value_count(data)).to eq({ :"/help_page/1" => 3, :"/contact" => 3 })
+    end
+  end
+
+  describe '#sorting,#unique_sorting,#visit_sorting,#all_sorting' do
+    it 'should take hash and return and sorting array' do
+      expect(sorter.sorting(unsorted_data, value)).to
+    end
+  end
+
+  describe '#sorting,#unique_sorting,#visit_sorting,#all_sorting' do
+    it 'should take hash and return and sorting array' do
+      expect(sorter.sorting(unsorted_data, value)).to eq(
+       [["/index", 8], ["/home", 8], ["/help_page/1", 8], ["/contact", 8], ["/about/2", 8], ["/about", 8]]
+       )
+    end
+  end
+
+  it "should return readable data" do
+    expect("a string").to match(/^\/(\w+)\/\d\s(\d+\-\w+)/)
+  end
+
 end
